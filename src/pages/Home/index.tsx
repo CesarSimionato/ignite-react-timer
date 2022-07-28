b8289325323670758ec05bcc988ba6c4986ffb7d
cesarmsimionato <cesar.simionato@sou.unifeob.edu.br>
2022-07-28 10:14:09 -0300

    construindo o formulario

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,10 @@
 import { Play } from "phosphor-react"
 
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+
+import * as zod from "zod"
+
 import {
   CountdownContainer,
   FormContainer,
@@ -10,16 +15,43 @@ import {
   TaskInput,
 } from "./styles"
 
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, "Informe a tarefa"),
+  minutesAmount: zod
+    .number()
+    .min(5, "O ciclo precisa ser no mínimo de 5 minutos")
+    .max(60, "O ciclo precisa ser no máximo de 60 minutos"),
+})
+
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
+
 export const Home: React.FC = () => {
+  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
+    resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: "",
+      minutesAmount: 0,
+    },
+  })
+
+  const handleCreateNewCycle = (data: NewCycleFormData) => {
+    console.log(data)
+    reset()
+  }
+
+  const task = watch("task")
+  const isSubmitDisabled = !task
+
   return (
     <HomeContainer>
-      <form action="">
+      <form onSubmit={handleSubmit(handleCreateNewCycle)}>
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
           <TaskInput
             id="task"
             list="task-suggestions"
             placeholder="Dê um nome para seu projeto"
+            {...register("task")}
           />
 
           <datalist id="task-suggestions">
@@ -27,7 +59,6 @@ export const Home: React.FC = () => {
             <option value="Projeto 2" />
             <option value="Projeto 3" />
             <option value="Projeto 4" />
-            <option value="Maça" />
           </datalist>
 
           <label htmlFor="minutesAmount">durante</label>
@@ -38,6 +69,7 @@ export const Home: React.FC = () => {
             step={5}
             min={5}
             max={60}
+            {...register("minutesAmount", { valueAsNumber: true })}
           />
 
           <span>minutos.</span>
@@ -51,7 +83,10 @@ export const Home: React.FC = () => {
           <span>0</span>
         </CountdownContainer>
 
-        <StartCountdownButton type="submit">
+        <StartCountdownButton
+          type="submit"
+          disabled={isSubmitDisabled}
+        >
           <Play size={24} />
           Começar
         </StartCountdownButton>
